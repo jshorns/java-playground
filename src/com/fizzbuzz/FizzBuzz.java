cb575b70c2dd481f8dc598dd403f0a81c90a6232
Joey <joey.s.hornsby@gmail.com>
2021-06-11 09:47:29 +0100

    refactoring if/else into series of guard clauses

--- src/com/fizzbuzz/FizzBuzz.java
+++ src/com/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,10 @@ package com.fizzbuzz;
 public class FizzBuzz {
 
     public static String fizzbuzz(int number) {
-        if (number == 0)
-            return "0";
-        else if (number % 15 == 0)
-            return "Fizzbuzz";
-        else if (number % 3 == 0)
-            return "Fizz";
-        else if (number % 5 == 0)
-            return "Buzz";
-        else
-            return Integer.toString(number);
+        if (number == 0) return "0";
+        if (number % 15 == 0) return "Fizzbuzz";
+        if (number % 3 == 0) return "Fizz";
+        if (number % 5 == 0) return "Buzz";
+        return Integer.toString(number);
     }
 }
